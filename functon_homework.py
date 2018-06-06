6e34788171c274f97eee93d697f86b907bb75900
Grace <1525436188@qq.com>
2018-06-06 20:49:40 +0800

    homework

---
+++ functon_homework.py
@@ -0,0 +1,39 @@
+def get_income(hot_dogs):
+	income = (6 * hot_dogs) - 3000
+	return income
+
+print(get_income(400))
+
+
+
+def get_income(n):
+	income = (6 * n) - 4
+	return income
+
+print(get_income(4))	
+print(get_income(5))	
+print(get_income(6))	
+print(get_income(7))	
+
+
+def food(b):
+	num =(1 / (3 ** b - 1))
+	return num
+
+print(food(1))	
+print(food(2))	
+print(food(3))	
+print(food(4))	
+print(food(5))	
+
+
+def f(a):
+	num = ( - 1 ** a) * (3)
+	return num
+
+
+print(f(1))	
+print(f(2))	
+print(f(3))	
+print(f(4))	
+print(f(5))	
